8e6f607b7c4540a70fd00352a91986b52945909c
SebagadisK <sebexk3@gmail.com>
2021-08-02 12:20:24 +0300

    Create template to show joke. Closes #5

--- src/main/java/com/springframework/spring5jokesapp/services/JokeServiceImpl.java
+++ src/main/java/com/springframework/spring5jokesapp/services/JokeServiceImpl.java
@@ -1,6 +1,7 @@
 package com.springframework.spring5jokesapp.services;
 
 import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -8,6 +9,7 @@ public class JokeServiceImpl implements JokeService {
 
     private final ChuckNorrisQuotes chuckNorrisQuotes;
 
+    @Autowired
     public JokeServiceImpl() {
         this.chuckNorrisQuotes = new ChuckNorrisQuotes();
     }
