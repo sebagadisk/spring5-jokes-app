59f62c3f7e7f6a1224a54dd6cf5f8ce4e967b1b5
SebagadisK <sebexk3@gmail.com>
2021-08-02 11:35:26 +0300

    Create the Joke Service. Closes #3

---
+++ src/main/java/com/springframework/spring5jokesapp/services/JokeService.java
@@ -0,0 +1,7 @@
+package com.springframework.spring5jokesapp.services;
+
+public interface JokeService {
+
+    String getJoke();
+
+}
---
+++ src/main/java/com/springframework/spring5jokesapp/services/JokeServiceImpl.java
@@ -0,0 +1,19 @@
+package com.springframework.spring5jokesapp.services;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JokeServiceImpl implements JokeService {
+
+    private final ChuckNorrisQuotes chuckNorrisQuotes;
+
+    public JokeServiceImpl() {
+        this.chuckNorrisQuotes = new ChuckNorrisQuotes();
+    }
+
+    @Override
+    public String getJoke() {
+        return chuckNorrisQuotes.getRandomQuote();
+    }
+}
